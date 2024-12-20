fb210559dec215f00a3eb6ee69cee55c156d34bf
Lukas <lukaskarstens07@gmail.com>
2024-12-20 11:26:18 +0100

    yes

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ const server = http.createServer(app);
 
 // Server listens on port
 server.listen(PORT, () => {
-  console.log(`listening on *:${PORT}`);
+    console.log(`listening on *:${PORT}`);
 });
 
 // Initialize Socket IO on server
 const { Server } = require("socket.io");
 const io = new Server(server, {
-  cors: {
-    origin: "http://127.0.0.1:5500",
-  },
+    cors: {
+        origin: "https://rive-animal.netlify.app/",
+    },
 });
 
 // Here we need to create the states for the rive animation and
@@ -24,21 +24,19 @@ const io = new Server(server, {
 let hp = 100;
 
 setInterval(() => {
-  hp = hp - 1;
+    hp = hp - 1;
 }, 1000);
 
 setInterval(() => {
-  io.sockets.emit("hp", { hp: hp });
+    io.sockets.emit("hp", { hp: hp });
 }, 200);
 
 // A user connects to the server (opens a socket)
 io.sockets.on("connection", function (socket) {
-  // Server recieves a ping and responds with an emit event (sends a message to all connected sockets)
-  io.sockets.emit("greet", { mesasge: "Server says hello" });
+    // Server recieves a ping and responds with an emit event (sends a message to all connected sockets)
+    io.sockets.emit("greet", { mesasge: "Server says hello" });
 
-  socket.on("hello", (data) => {
-    console.log("Recieved client ping: ", data);
-
-
-  });
+    socket.on("hello", (data) => {
+        console.log("Recieved client ping: ", data);
+    });
 });
